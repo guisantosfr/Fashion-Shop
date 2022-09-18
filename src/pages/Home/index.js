d1ccf91c49adc5d943d43f71a8c42778e680ca67
Guilherme Santos <santosgui678@gmail.com>
2022-09-18 12:00:13 -0300

    Footer da página inicial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,13 @@
 import { useEffect } from 'react';
 
 import Banner from '../../components/Banner';
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import NavBar from '../../components/NavBar';
 import SearchBar from '../../components/SearchBar';
 import Section from '../../components/Section';
 
-/*
-import Footer from '../../components/Footer';
-*/
-
 function Home() {
-  useEffect(() => {
-    // recupera o produtos pela api usando fetch
-  }, []);
-
   return (
     <div className="container">
       <Header />
@@ -24,7 +17,7 @@ function Home() {
       <Section titulo='Ofertas' />
       <Section titulo='Feminino' />
       <Section titulo='Masculino' />
-      {/*<Footer /> */}
+      <Footer />
     </div>
   );
 }
